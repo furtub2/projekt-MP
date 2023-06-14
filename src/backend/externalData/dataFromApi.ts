5fa88ef8310ebda947205a1d3b950b134c30071c
Jarek Bigaj <j.bigaj@interia.pl>
2023-06-14 13:30:44 +0200

    update endpoint get cities

--- src/backend/externalData/dataFromApi.ts
+++ src/backend/externalData/dataFromApi.ts
@@ -2,8 +2,21 @@ import axios from "../api/axiosGeocoding";
 
 const SEARCH_PARAMS = `search?name=`;
 
+interface Location {
+    name:string;
+    country:string;
+    longitude:number;
+    latitude:number
+}
+
 export const getCitiesByName = async (cityName:string) =>{
     const response = await axios.get(`${SEARCH_PARAMS}${cityName}`);
     const {data} = response;
-    return data;
+    const {results} = data;
+    const locations: Location[] = results.map((location:Location) => {
+        const {name,country,longitude,latitude} = location;
+        return {name,country,longitude,latitude};
+    })
+
+    return locations;
 }
