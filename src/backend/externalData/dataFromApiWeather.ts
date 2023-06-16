40f246467021989a44226fabb9adb1653df1e15c
Jarek Bigaj <j.bigaj@interia.pl>
2023-06-16 19:02:27 +0200

    change url logic

--- src/backend/externalData/dataFromApiWeather.ts
+++ src/backend/externalData/dataFromApiWeather.ts
@@ -1,32 +1,22 @@
-import axios, { AxiosError, isAxiosError } from "axios";
 import axiosWeather from "../api/axiosWeather";
-
-const URL_PARAM_FORECAST = `forecast?`;
-const URL_PARAM_CURRENT_WEATHER = `&current_weather=true`;
-const URL_PARAM_LATITUDE = `latitude=`;
-const URL_PARAM_LONGITUDE = `&longitude=`;
-const URL_PARAM_DAILY_WEATHER = `&daily=temperature_2m_max,apparent_temperature_max,windspeed_10m_max`;
-const URL_DAYS = `&forecast_days=`;
-const URL_PARAM_TIMEZONE = `&timezone=Europe%2FBerlin`;
-
+import { getUrl } from "../api/urlForApi";
 
 
 export const getCurrentWeatherFromSelectedCity = async (long:number,lat:number) =>{
-    const urlForCurrentWeather = `${URL_PARAM_FORECAST}${URL_PARAM_LATITUDE}${lat}${URL_PARAM_LONGITUDE}${long}${URL_PARAM_CURRENT_WEATHER}`;
-    const response = await axiosWeather(urlForCurrentWeather);
+    const url = getUrl(lat,long);
+    const response = await axiosWeather(url);
     const {data} = response;
     const {current_weather} = data;
     return current_weather;
 }
 
 export const getWeatherForSeveralDaysForSelectedCity = async (long:number,lat:number,days:number) => {
-    const urlForWeather = URL_PARAM_FORECAST+
-        URL_PARAM_LATITUDE+lat+
-        URL_PARAM_LONGITUDE+long+
-        URL_PARAM_DAILY_WEATHER+
-        URL_DAYS+days+
-        URL_PARAM_TIMEZONE;
-    const response = await axiosWeather(urlForWeather);
+    const url = getUrl(lat,long,days)
+    const response = await axiosWeather(url);
     const {data} = response;
     return data;
 }
+
+// export const getCurrentWeatherForSelectedCities = async (listOfLongitude:number[],listOfLatitude:number[]) =>{
+//     const urlFor
+// }
