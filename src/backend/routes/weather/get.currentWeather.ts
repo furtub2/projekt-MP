f73e690cbf101f80b1234d418814797770dc40a3
Wojciech Batorski <wojbat4@gmail.com>
2023-06-17 00:38:22 +0200

    Refactor getCurrentWeather endpoint

--- src/backend/routes/weather/get.currentWeather.ts
+++ src/backend/routes/weather/get.currentWeather.ts
@@ -1,22 +1,31 @@
-import { RequestHandler } from "express";
+import {Request, response, Response} from "express";
 import { getCurrentWeatherFromSelectedCity } from "../../externalData/dataFromApiWeather";
 import { LATITUDE_REGEX, LONGITUDE_REGEX } from "../../validations/regexExpression";
+import {authorize} from "../../utils/middleware.utils";
+import {handleRequest} from "../../utils/request.utils";
+import {StatusCodes} from "http-status-codes";
+import {TRoute} from "../types";
 
 
-export const getCurrentWeather: RequestHandler = (req,res) =>{
-    try{
-        const {latitude,longitude} = req.body;
+export default {
+    method: 'get',
+    path: '/api/currentWeather',
+    validators: [authorize],
+    handler: async (req: Request, res: Response) =>
+        handleRequest({
+            req,
+            res,
+            responseSuccessStatus: StatusCodes.OK,
+            messages: {},
+            execute: async () => {
+                const {latitude,longitude} = req.body;
 
-        if(!latitude || !longitude) throw new Error("Please provide values for latitude and longitude.")
+                if(!latitude || !longitude) throw new Error("Please provide values for latitude and longitude.")
 
-        if (!LATITUDE_REGEX.test(latitude) || !LONGITUDE_REGEX.test(longitude)) 
-            throw new Error("Invalid longitude or latitude. Please try again!");
-        (async() =>{
-            const response = await getCurrentWeatherFromSelectedCity(longitude,latitude);
-            res.send(response);
-        })()
-    }catch(err){
-        console.log(err);
-        res.status(400).send({ error: `${err}`}); 
-    }
-}
+                if (!LATITUDE_REGEX.test(latitude) || !LONGITUDE_REGEX.test(longitude))
+                    throw new Error("Invalid longitude or latitude. Please try again!");
+
+                return await getCurrentWeatherFromSelectedCity(longitude, latitude);
+            },
+        }),
+} as TRoute;
